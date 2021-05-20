417d0dfde1e6c4f0b508163792bea6dc81f989cd
ejPark <qdw0313@khu.ac.kr>
2021-05-20 15:50:55 +0900

    shell

---
+++ tutorial.sh
@@ -0,0 +1,5 @@
+django-admin startproject Dtutorial
+# manage.py ; Django 프로젝트와 다양한 방법으로 상호작용하는 CLI의 유틸리티
+#             추가정보 https://docs.djangoproject.com/ko/3.2/ref/django-admin/
+# [ProjectName]/ ; 프로젝트를 위한 실제 Python 패키지([ProjectName].urls로 import 가능)
+# [ProjectName]/__init__.py
